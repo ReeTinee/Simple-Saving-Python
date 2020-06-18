a99a2e7672dcfb323755909a865fd5c0c365ab5f
ReeTinee <53974414+ReeTinee@users.noreply.github.com>
2020-06-19 01:11:56 +0200

    Update READ ME.py

--- READ ME.py
+++ READ ME.py
@@ -1,11 +1,11 @@
 #/!\ It's important to respect quoting marks#
 
-	#For import the package :
+	#To import the package :
 from Saving import dumping,loading
 	
-	#For dumping the variable B in doc Save.txt :
+	#To save the variable B in doc Save.txt :
 dumping(B,"Save.txt")
 
-	#For loading the variable B from the doc Save.txt :
+	#To load the variable B from the doc Save.txt :
 B = loading("B","Save.txt")
 
