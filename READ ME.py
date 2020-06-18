746d155eaaccb17120fad9fac7b2e32d50d450e8
ReeTinee <53974414+ReeTinee@users.noreply.github.com>
2020-06-19 01:13:00 +0200

    Update READ ME.py

--- READ ME.py
+++ READ ME.py
@@ -3,7 +3,7 @@
 	#To import the package :
 from Saving import dumping,loading
 	
-	#To save the variable B in doc Save.txt :
+	#To save the variable B in file Save.txt (if Save.txt doesn't exist, it'll be created) :
 dumping(B,"Save.txt")
 
 	#To load the variable B from the doc Save.txt :
